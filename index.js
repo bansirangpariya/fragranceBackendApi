8ae941b7139111c2ce26bcf09d1177c2516e95f9
bansi <bansirangpariya@gmail.com>
2024-11-26 10:33:45 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const productRoute = require("./routes/product");
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/perfume", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello, this is your response from port 8000!");
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
